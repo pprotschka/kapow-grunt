ba6b6b69019df1180f3dc906f4ff12ec893b7e00
Dave Green <hello@davetgreen.me>
2015-06-08 18:06:33 +0100

    Update jshint.js
    Refactored as force: true is a task level option.

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -2,14 +2,15 @@ module.exports = {
   options: {
     jshintrc: 'grunt/config/jshintrc.json',
     reporter: require('jshint-stylish'),
-    reporterOutput: '<%= siteInfo.reports_path %>/jshint.txt',
-    force: true
+    reporterOutput: '<%= siteInfo.reports_path %>/jshint.txt'
   },
+  scripts: {
     options: {
-      reporterOutput: '<%= siteInfo.reports_path %>/jshint.txt'
+      force: true
     },
     src: [
     '<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/**/*.js',
     '!<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/lib/modernizr-custom.js'
-    ]
+    ] 
+  }
 };
