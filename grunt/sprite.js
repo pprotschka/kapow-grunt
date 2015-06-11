41c5940df88f0001915d3332b0cac85d79c0266d
Dave Green <hello@davetgreen.me>
2015-06-11 15:36:54 +0100

    Update sprite.js
    Corrected error -  incorrect sprite destination

--- grunt/sprite.js
+++ grunt/sprite.js
@@ -4,7 +4,7 @@ module.exports = {
       '<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>/*.png',
       '!<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>/sprite.png'
     ],
-    dest: '<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>/sprite.png',
+    dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.img_dir %>/sprite.png',
     destCss: '<%= siteInfo.assets_path %>/<%= siteInfo.sass_dir %>/base/_sprites.scss'
   }
 };
