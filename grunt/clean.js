fb24da6636b7870654e4de35f282f152fd958c08
Dave Green <hello@davetgreen.me>
2015-06-15 11:33:41 +0100

    Update clean.js

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,7 +1,7 @@
 module.exports = {
   scripts: {
     src: [
-    '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.js_dir %>/*.tmp.js'
+    '<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/*.tmp.js'
     ]
   }
 };
