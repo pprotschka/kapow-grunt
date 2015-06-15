fd02a6eb9319e8bdb5d83661688509361c48af82
Dave Green <hello@davetgreen.me>
2015-06-15 11:29:23 +0100

    Update clean.js
    Altered cleanup to only get rid of tmp js files.

--- grunt/clean.js
+++ grunt/clean.js
@@ -1,8 +1,7 @@
 module.exports = {
   scripts: {
     src: [
-    '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.js_dir %>/*.js',
-    '!<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.js_dir %>/*.min.js'
+    '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.js_dir %>/*.tmp.js'
     ]
   }
 };
