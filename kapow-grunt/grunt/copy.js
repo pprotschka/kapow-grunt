697080a078fdc80fd8a3e2ca5bca02adfaf1e784
Dave Green <hello@davetgreen.me>
2015-09-17 14:21:20 +0100

    Added sourcemap copying tasks

--- kapow-grunt/grunt/copy.js
+++ kapow-grunt/grunt/copy.js
@@ -1,10 +1,16 @@
 // Copy Task - https://github.com/gruntjs/grunt-contrib-copy
 // ----------------------------------------------------------------------------
 module.exports = {
-  // copy: {
-  //   expand: true,
-  //   cwd: 'path/to/files/',
-  //   src: ['filename'],
-  //   dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/path/'
-  // }
+  main_sourcemap: {
+    expand: true,
+    cwd: '<%= siteInfo.assets_path %>/<%= siteInfo.css_dir %>/',
+    src: [ 'style.css.map' ],
+    dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/'
+  },
+  other_sourcemaps: {
+    expand: true,
+    cwd: '<%= siteInfo.assets_path %>/<%= siteInfo.css_dir %>/',
+    src: [ '*.map', '!style.css.map' ],
+    dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/css'
+  }
 };
