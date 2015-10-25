4dc932b4d0d36678ef58f4a73a3513e6a618dcf0
Dave Green <hello@davetgreen.me>
2015-10-25 21:13:28 +0000

    Commented out sync task - needs to be optional

--- kapow-grunt/grunt/default.js
+++ kapow-grunt/grunt/default.js
@@ -2,7 +2,7 @@
 // ----------------------------------------------------------------------------
 module.exports = function( grunt ) {
 	grunt.registerTask( 'default', [
-		'sync',
+		// 'sync',
 		// "lint",
 		"sass",
 		"postcss",
