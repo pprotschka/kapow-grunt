10bffee20413fac23fe43c2e8849d6f019d0eacf
Dave Green <hello@davetgreen.me>
2015-11-10 10:47:57 +0000

    Fix

--- kapow-grunt/grunt/selector4096.js
+++ kapow-grunt/grunt/selector4096.js
@@ -1,7 +1,6 @@
 // Selector 4096 Task - https://github.com/hail2u/grunt-selector4096
 // ----------------------------------------------------------------------------
 module.exports = {
-	selector4096: {
-		style: [ '<%= siteInfo.assets_path %>/<%= siteInfo.css_dir %>/<%= siteInfo.sass_file %>.css' ],
-	}
+
+	style: [ '<%= siteInfo.assets_path %>/<%= siteInfo.css_dir %>/<%= siteInfo.sass_file %>.css' ],
 };
