a9b6eb0bec37c4aee00f1d279ce4c5b7d11be642
Dave Green <hello@davetgreen.me>
2015-12-16 12:18:49 +0000

    Bug fix with dest path
    Spritesmith was still saving to theme/img not theme/assets/img

--- kapow-grunt/grunt/sprite.js
+++ kapow-grunt/grunt/sprite.js
@@ -7,7 +7,7 @@ module.exports = {
 			'<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>/*.png',
 			'!<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>/sprite.png'
 		],
-		dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.img_dir %>/sprite.png',
+		dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.img_dir %>/<%= wpInfo.assets_dir %>/sprite.png',
 		destCss: '<%= siteInfo.assets_path %>/<%= siteInfo.sass_dir %>/base/_sprites.scss',
 		// Add a cache busting timestamp to the file name
 		imgPath: '<%= wpInfo.assets_path %>/<%= wpInfo.img_dir %>/sprite.png?' + ( new Date().getTime() ),
