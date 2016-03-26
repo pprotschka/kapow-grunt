6f94de70e50754100679a2e5024f966cfd8feb79
Dave Green <hello@davetgreen.me>
2016-03-26 12:05:09 +0000

    Fixed bug with duplicate sprite PNGs

--- kapow-grunt/grunt/sprite.js
+++ kapow-grunt/grunt/sprite.js
@@ -7,7 +7,8 @@ module.exports = {
 			'<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>/*.png',
 			'!<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>/sprite.png'
 		],
-		dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.assets_dir %>/<%= wpInfo.img_dir %>/sprite.png?' + ( new Date().getTime() ),
+		dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.assets_dir %>/<%= wpInfo.img_dir %>/sprite.png',
 		destCss: '<%= siteInfo.assets_path %>/<%= siteInfo.sass_dir %>/base/_sprites.scss',
+		imgPath: '<%= wpInfo.assets_path %>/<%= wpInfo.img_dir %>/sprite.png?' + ( new Date().getTime() ),
 	}
 };
