e6bb0f15d1cbb49e17ccf3c2deb883175008b270
Dave Green <hello@davetgreen.me>
2016-03-27 18:56:34 +0100

    Added svgmin config

---
+++ kapow-grunt/grunt/svgmin.js
@@ -0,0 +1,26 @@
+// SVG Min Task - https://github.com/sindresorhus/grunt-svgmin
+// ----------------------------------------------------------------------------
+module.exports = {
+	options: {
+		plugins: [
+			{
+				removeTitle: true
+			},
+			{
+				removeDimensions: true
+			},
+			{
+				removeStyleElement: true
+			},
+			{
+				sortAttrs: true
+			}
+		]
+	},
+	files: [ {
+		expand: true,
+		cwd: '<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>',
+		src: [ '**/*.svg' ],
+		dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.assets_dir %>/<%= wpInfo.img_dir %>'
+	} ]
+};
