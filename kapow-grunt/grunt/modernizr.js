d42355d58c17d00a4cc87f56245ff990c0cc408d
Dave Green <hello@davetgreen.me>
2016-03-31 23:57:08 +0100

    Excluded Modernizr's hidden attributes test to prevent the .hidden class being added to the <html> element

--- kapow-grunt/grunt/modernizr.js
+++ kapow-grunt/grunt/modernizr.js
@@ -1,22 +1,32 @@
 // Modernizr Task - https://github.com/Modernizr/grunt-modernizr
 // ----------------------------------------------------------------------------
 module.exports = {
-	// Generate a custom Modernizr build based on checks found in our Sass & JS
-	custom: {
-		"cache": true,
-		"uglify": false,
-		"options": [
-			"setClasses"
-		],
-		"devFile": "bower_components/modernizr/modernizr.js",
-		"dest": "<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/lib/modernizr-custom.js",
-		"crawl": true,
-		"files": {
-			"src": [
-				'<%= siteInfo.assets_path %>/<%= siteInfo.sass_dir %>/**/*.scss',
-				'<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/**/*.js',
-				'!<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/lib/modernizr-custom.js'
-			]
-		}
-	}
+    // Generate a custom Modernizr build based on checks found in our Sass & JS
+    custom: {
+        "cache": true,
+        "uglify": false,
+        "options": [
+            "domPrefixes",
+            "prefixes",
+            "mq",
+            "prefixed",
+            "testAllProps",
+            "testProp",
+            "setClasses"
+        ],
+        "tests": [],
+        "excludeTests": [
+            "hidden"
+        ],
+        "devFile": "bower_components/modernizr/modernizr.js",
+        "dest": "<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/lib/modernizr-custom.js",
+        "crawl": true,
+        "files": {
+            "src": [
+                '<%= siteInfo.assets_path %>/<%= siteInfo.sass_dir %>/**/*.scss',
+                '<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/**/*.js',
+                '!<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/lib/modernizr-custom.js'
+            ]
+        }
+    }
 };
