b7767d411efa664dcb4b336fab85bb2147bcce83
Dave Green <hello@davetgreen.me>
2016-04-02 15:39:33 +0100

    Comments update

--- kapow-grunt/grunt/default.js
+++ kapow-grunt/grunt/default.js
@@ -1,4 +1,5 @@
-// Default Task - The master task used to build/re-build the project
+// Default Task - The master task used to build/re-build the project. Comment,
+// un-comment or re-arrange to suit your specific needs.
 // ----------------------------------------------------------------------------
 module.exports = function( grunt ) {
 	grunt.registerTask( 'default', [
