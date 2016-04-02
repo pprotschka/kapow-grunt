9b6e8fa2714d477523e5b5708e60b8428094a017
Dave Green <hello@davetgreen.me>
2016-04-02 16:13:15 +0100

    Tidied up formatting

--- kapow-grunt/grunt/svgmin.js
+++ kapow-grunt/grunt/svgmin.js
@@ -1,28 +1,23 @@
 // SVG Min Task - https://github.com/sindresorhus/grunt-svgmin
 // ----------------------------------------------------------------------------
 module.exports = {
-	options: {
-		plugins: [
-			{
-				removeTitle: true
-			},
-			{
-				removeDimensions: true
-			},
-			{
-				removeStyleElement: true
-			},
-			{
-				sortAttrs: true
-			}
-		]
-	},
-	svg: {
-		files: [ {
-			expand: true,
-			cwd: '<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>',
-			src: [ '**/*.svg' ],
-			dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.assets_dir %>/<%= wpInfo.img_dir %>'
-		} ]
-	}
+    options: {
+        plugins: [{
+            removeTitle: true
+        }, {
+            removeDimensions: true
+        }, {
+            removeStyleElement: true
+        }, {
+            sortAttrs: true
+        }]
+    },
+    svg: {
+        files: [{
+            expand: true,
+            cwd: '<%= siteInfo.assets_path %>/<%= siteInfo.img_dir %>',
+            src: ['**/*.svg'],
+            dest: '<%= wpInfo.wp_content %>/themes/<%= wpInfo.theme_name %>/<%= wpInfo.assets_dir %>/<%= wpInfo.img_dir %>'
+        }]
+    }
 };
