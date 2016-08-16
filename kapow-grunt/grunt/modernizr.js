ee5a3d5460e6d7c0aba478131868d8364721f85d
Dave Green <hello@davetgreen.me>
2016-08-16 23:20:27 +0100

    Bug - missing comma

--- kapow-grunt/grunt/modernizr.js
+++ kapow-grunt/grunt/modernizr.js
@@ -18,7 +18,7 @@ module.exports = {
             "setClasses"
         ],
         "tests": [
-            "flexbox,"
+            "flexbox",
             "cssanimations",
             "cssgradients",
             "csstransforms",
