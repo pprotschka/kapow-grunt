f1ab2021fca043d8ac7637d9a7dcd7d3165a68df
Dave Green <hello@davetgreen.me>
2017-05-29 19:31:11 +0100

    Tweaks to concat.

--- kapow-grunt/grunt/concat.js
+++ kapow-grunt/grunt/concat.js
@@ -9,35 +9,38 @@ module.exports = {
 	header: {
 		src: ['<%= concatHead %>'],
 		dest: '<%= wpInfo.wp_content %>/<%= wpInfo.themes_dir %>/<%= wpInfo.theme_name %>/<%= wpInfo.assets_dir %>/<%= wpInfo.js_dir %>/header.js',
-		nonull: true
+		nonull: false
 	},
 	header_min: {
 		src: ['<%= concatHead %>'],
 		dest: '<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/header.tmp.js',
-		nonull: true
+		nonull: false
 	},
 	// Header JS for IE.
 	// -------------------------------------
 	header_ie: {
-		src: ['<%= concatHeadIE %>'],
+		src: [
+			'<%= concatHeadIE %>',
+			'<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/header_ie.js'
+		],
 		dest: '<%= wpInfo.wp_content %>/<%= wpInfo.themes_dir %>/<%= wpInfo.theme_name %>/<%= wpInfo.assets_dir %>/<%= wpInfo.js_dir %>/header_ie.js',
-		nonull: true
+		nonull: false
 	},
 	header_ie_min: {
 		src: ['<%= concatHeadIE %>'],
 		dest: '<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/header_ie.tmp.js',
-		nonull: true
+		nonull: false
 	},
 	// Footer JS.
 	// -------------------------------------
 	footer: {
 		src: ['<%= concatFoot %>'],
 		dest: '<%= wpInfo.wp_content %>/<%= wpInfo.themes_dir %>/<%= wpInfo.theme_name %>/<%= wpInfo.assets_dir %>/<%= wpInfo.js_dir %>/footer.js',
-		nonull: true
+		nonull: false
 	},
 	footer_min: {
 		src: ['<%= concatFoot %>'],
 		dest: '<%= siteInfo.assets_path %>/<%= siteInfo.js_dir %>/footer.tmp.js',
-		nonull: true
+		nonull: false
 	}
 };
